de35cafa10308d6094e1a74ff62255d2d8b8cf99
dazer007 <ab601026460@163.com>
2023-04-07 18:34:26 +0800

    deploy-gitee.sh 脚本测试成功

--- deploy-gitee.sh
+++ deploy-gitee.sh
@@ -4,7 +4,7 @@
 set -e
 
 # 生成文档
-#yarn run build
+yarn run build
 
 # 输出 CNAME 文件
 echo "dazer007.gitee.io" > CNAME
@@ -12,14 +12,14 @@ echo "github.ityun.ltd" >> CNAME
 echo "google.com, pub-4147143076931995, DIRECT, f08c47fec0942fa0" > ads.txt
 
 # personal access tokens 私人令牌
-GIT_TOKEN=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 #your token, 以前可用通过账号密码验证，现在必须是token更安全。
+GIT_TOKEN=093101d52304336cf8a0b8c205b01abc #your token, 以前可用通过账号密码验证，现在必须是token更安全。
 GIT_USERNAME=dazer007 #账号，owner,username
-GIT_REPO=dazer007.github.io #仓库名称,repo
+GIT_REPO=dazer007 #仓库名称,repo
 
 # e.coding.net
 # github.com    https://Github生成的personal-access-tokens@github.com/github用户名/代码仓库名.git
 # gitee.com     https://码云用户名:勾选projects的私人令牌@gitee.com/码云用户名/代码仓库名.git
-push_addr=https://${GIT_TOKEN}@github.com/${GIT_USERNAME}/${GIT_REPO}.git
+push_addr=https://${GIT_USERNAME}:${GIT_TOKEN}@gitee.com:/${GIT_USERNAME}/${GIT_REPO}.git
 commit_info="`git describe --all --always --long`',来自Github Actions的自动部署，更新于'$(TZ=UTC-8 date "+%Y-%m-%d %H:%M:%S")"
 dist_path=docs/.vuepress/dist # 打包生成的文件夹路径
 push_branch=gh-pages # 推送的分支
@@ -33,6 +33,7 @@ push_branch=gh-pages # 推送的分支
  git init
  git add -A
  git commit -m "deploy, $commit_info"
- git remote add origin $push_addr # 添加远程推送的仓库, 如：origin是一个名称，可用自行替换。
- git push -f origin HEAD:$push_branch
+
+ git remote add gitee $push_addr # 添加远程推送的仓库, 如：origin是一个名称，可用自行替换。
+ git push -f gitee HEAD:$push_branch
 
